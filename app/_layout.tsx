9aa48281f879014437105ca41aa80d14250173ce
Emirhan <59915877+swempish@users.noreply.github.com>
2024-08-08 01:27:18 +0300

    minor

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
