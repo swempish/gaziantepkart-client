c2cc2e2456d7e087b3000e9299ed9f0c52f8802c
Emirhan <59915877+swempish@users.noreply.github.com>
2024-08-07 23:49:12 +0300

    Init

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,34 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          height: 60,
+        },
+        headerStyle: {
+          marginBottom: 20,
+        },
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Anasayfa',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/gaziantep.jpg')}
+          style={{resizeMode: 'cover', height: 250, width: '100%'}}
+        />
+      }>
+
+      <Text style={{color: 'white', textAlign: 'left', fontWeight: 'bold', fontSize: 24}}>Gaziantep Kart</Text>
+        
+    </ParallaxScrollView>
+  );
+}
