bdc3f50513aeba26d68427a57555a74a1ea5ac33
Emirhan <59915877+swempish@users.noreply.github.com>
2024-08-26 10:26:13 +0300

    Tarifeler listesi eklendi, ona göre kalan biniş hesaplama eklendi

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,9 +16,11 @@ export default function HomeScreen() {
   const [unMaskedValue, setUnmaskedValue] = useState("");
 
   const [proccessing, setProcessing] = useState(false);
-  const [response, setResponse] = useState({ "cardlist": null });
+  const [response, setResponse] = useState({ "cardlist": null, "clientMeta": null });
 
   const [duyurular, setDuyurular] = useState();
+
+  const [tarifeler, setTarifeler] = useState({"clientMeta": {"öğrenci": 0,"tam": 0}});
 
   const [mevcutDuyuru, setMevcutDuyuru] = useState();
 
@@ -47,6 +49,12 @@ export default function HomeScreen() {
       "referrer": "https://m.kentkart.com/",
     }
 
+    /**
+     * Usage Listesi sonucu
+     * type : 0 = Karttan para çekilmiş, kart kullanılmış
+     * type : 1 = Karta para yüklenmiş bekleyen işlem
+     */
+
     // Create the URL with query parameters
     let urlWithParams = new URL(url);
     for (const [key, value] of Object.entries(params)) {
@@ -69,7 +77,19 @@ export default function HomeScreen() {
         if (data.result.code == 33) {
           alert(data.result.message);
         }
-        console.log(data);
+        data["clientMeta"] = {
+          "bakiyeİşlemi": false,
+          "miktar": 0,
+          "mesaj": ""
+        };
+        
+        if (data.cardlist[0].oChargeMessage && data.cardlist[0].oChargeMessage != "") {
+          data["clientMeta"]["bakiyeİşlemi"] = true;
+          data["clientMeta"]["mesaj"] = "Henüz gerçekleşmemiş dolumunuz mevcuttur!\r\n\r\n"+ data.cardlist[0].oChargeList[0].datetime +" - "+ String(data.cardlist[0].oChargeList[0].amount) +" TL\r\n\r\n";
+          data["clientMeta"]["miktar"] = data.cardlist[0].oChargeList[0].amount;
+        }
+
+        
         setResponse(data);
         setProcessing(false);
       })
@@ -124,8 +144,95 @@ export default function HomeScreen() {
 
   }
 
+  async function tarifeleriGetir() {
+    let url = "https://service.kentkart.com/rl1//api/info/tariff"
+    let params = {
+      "region": "028",
+      "version": "Web_1.7.2(24)_1.0_FIREFOX_kentkart.web.mkentkart",
+      "lang": "tr"
+    }
+
+    let headers = {
+      "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1;) AppleWebKit/601.21 (KHTML, like Gecko) Chrome/50.0.2957.192 Safari/600",
+      "Accept": "application/json, text/plain, */*",
+      "Accept-Language": "tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3",
+      "Content-Type": "application/json",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-site",
+      "If-None-Match": "W/\"14b9-YoL7VFLSzqNR1AGKjCZb/Dh5jMc\""
+    }
+
+    // Create the URL with query parameters
+    let urlWithParams = new URL(url);
+    for (const [key, value] of Object.entries(params)) {
+      urlWithParams.searchParams.append(key, value);
+    }
+
+    fetch(urlWithParams, {
+      method: 'GET',
+      headers: headers
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Error: ${response.status}, ${response.statusText}`);
+      }
+    })
+    .then(data => {
+
+      let temizData = {"clientMeta": {
+        "öğrenci": 0,
+        "tam": 0
+      }};
+      temizData["clientMeta"] = {
+        "öğrenci": 0,
+        "tam": 0
+      };
+
+      // Regex ifadesi
+      let regex = /Özel\s?Halk\s?Otobüsü\s?:\s?(\d+(?:\.\d+)?)₺/;
+
+      for (let index = 0; index < data.tariffList.length; index++) {
+        const tarife = data.tariffList[index];
+        let text = tarife.description;
+
+        if (tarife["id"] == 14 /* Öğrenci */) {
+          // Eşleşmeyi bul
+          let match = text.match(regex);
+          
+          if (match) {
+            temizData["clientMeta"]["öğrenci"] = parseFloat(match[1]);
+          }
+        } else if(tarife["id"] == 16 /* Tam */) {
+          
+          let match = text.match(regex);
+          if (match) {
+            temizData["clientMeta"]["tam"] = parseFloat(match[1]);
+          }
+        } else {
+          continue;
+        }
+      }
+      
+
+      console.log(JSON.stringify(temizData, null, 4));
+      setTarifeler(temizData);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+    
+  }
+
   useEffect(() => {
-    duyurularıGetir();
+    async function fetchData() {
+      await duyurularıGetir();
+      await tarifeleriGetir();
+    }
+    fetchData();
   }, []);
 
   const [isModalVisible, setModalVisible] = useState(false);
@@ -177,6 +284,9 @@ export default function HomeScreen() {
 
         </ScrollView>
 
+        <Text style={{ color: 'white', textAlign: 'left', fontWeight: 'bold', fontSize: 24, marginBottom: 10, marginLeft: 10 }}>Tarifeler</Text>
+        <Text style={{ color: 'white', textAlign: 'left', fontSize: 15, marginBottom: 10, marginLeft: 10 }}>{"Öğrenci: " + tarifeler["clientMeta"]["öğrenci"] + " TL\nTam: " + tarifeler["clientMeta"]["tam"] + " TL\n"}</Text>
+
         <View style={{ flex: 1, borderColor: 'gray', borderWidth: 1, borderRadius: 5, padding: 10, minHeight: 100 }}>
           <Text style={{ color: 'white', textAlign: 'left', fontWeight: 'bold', fontSize: 24 }}>Bakiye sorgula</Text>
           <MaskedTextInput mask="99999-99999-9" onChangeText={(text, rawText) => { setMaskedValue(text); setUnmaskedValue(rawText); }} keyboardType='numeric' placeholder="Kart numarası. Örn. 12345-12345-1" style={{ color: 'black', textAlign: 'left', fontSize: 15, backgroundColor: 'white', height: 40, borderRadius: 5, marginVertical: 10, paddingHorizontal: 10 }} />
@@ -195,6 +305,9 @@ export default function HomeScreen() {
           <View style={{ marginTop: 20, flex: 1, borderColor: 'gray', borderWidth: 1, borderRadius: 5, padding: 10, minHeight: 100, flexDirection: 'column', justifyContent: 'space-between' }}>
             <Text style={{ color: 'white', textAlign: 'left', fontWeight: 'bold', fontSize: 24 }}>Kart bilgileri</Text>
             <View style={{ flexDirection: 'column', justifyContent: 'space-between', gap: 10, marginTop: 10 }}>
+              { response["clientMeta"]!["bakiyeİşlemi"] && (
+                <Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>{response["clientMeta"]!["mesaj"]}</Text>
+              ) }
               <View style={{  padding: 10, minHeight: 100 }}>
                 <Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>Kart Numarası</Text>
                 <Text style={{ color: '#0a7ea4', textAlign: 'center', fontWeight: 'bold', fontSize: 24 }}>{maskedValue}</Text>
@@ -207,7 +320,7 @@ export default function HomeScreen() {
 
               <View style={{  padding: 10, minHeight: 100 }}>
                 <Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>Kalan Kullanım</Text>
-                <Text style={{ color: '#388E3C', textAlign: 'center', fontWeight: 'bold', fontSize: 24 }}>{Math.floor(response['cardlist']![0]['balance'] / response['cardlist']![0]['usage'][0]["amt"])} Biniş</Text>
+                <Text style={{ color: '#388E3C', textAlign: 'center', fontWeight: 'bold', fontSize: 24 }}>{ tarifeler["clientMeta"]["öğrenci"] != 0 ? Math.floor(response['cardlist']![0]['balance'] / tarifeler["clientMeta"]["öğrenci"]) : " - " } Biniş</Text>
               </View>
 
               <View style={{  padding: 10, minHeight: 100 }}>
